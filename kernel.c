0a8a785ed83842bda00c000aa83728868dcfce40
Ian Page Hands <iphands@gmail.com>
2009-12-08 21:46:57 -0500

    kernel.c: working on video buffer management

--- kernel.c
+++ kernel.c
@@ -1,5 +1,7 @@
-void printk(char * my_string);
-void video_test();
+void printk(char * my_string, unsigned short int line);
+void push_line(char * line, unsigned char * vid_buffer);
+void flush_video(char c, unsigned char * vid_buffer);
+void write_buff(unsigned char * vid_buffer);
 
 void kmain( void* mbd, unsigned int magic )
 {
@@ -18,39 +20,66 @@ void kmain( void* mbd, unsigned int magic )
   /* or do your offsets yourself. The following is merely an example. */ 
   char * boot_loader_name =(char*) ((long*)mbd)[16];
  
-  /* Print a letter to screen to see everything is working: */
-  unsigned char *videoram = (unsigned char *) 0xb8000;
 
-  //videoram[0] = 65; /* character 'A' */
-  printk("Hello World");
-  //video_test();
-  videoram[1] = 0x07; /* forground, background color. */
- 
   /* Write your kernel here. */
+  unsigned char vid_buffer[2000];
+  flush_video(' ', vid_buffer);
+  
+  printk("Hello World", 0);
+  printk("Hello World", 0);  
+  
+  push_line("Hello World", vid_buffer);
+  push_line("This is a test", vid_buffer);
+  push_line("Hello World", vid_buffer);
+  
+  //unsigned char *videoram = (unsigned char *) 0xb8000;
+  //videoram[1920 * 2] = 'A';
 }
 
-void video_test()
+void push_line(char * line, unsigned char * vid_buffer)
+{
+
+  for(int i = 0; i < 25; i++)
+    {
+      for(int j = 0; j < 80; j++)
+	{
+	  
+	}
+    }
+
+  //write_buff();
+  printk(line, (80 * 24) * 2);
+}
+
+void write_buff(unsigned char * vid_buffer)
+{
+  unsigned char * videoram = (unsigned char *) 0xb8000;
+  for(int i = 0; i < 4000; i++)
+    videoram[i] = vid_buffer[i];
+}
+
+void flush_video(char c, unsigned char * vid_buffer)
 {
   unsigned short int i = 0;
-  unsigned char *videoram = (unsigned char *) 0xb8000;
-  while(i < 2000)
+  unsigned char * videoram = (unsigned char *) 0xb8000;
+  while(i < (2000 * 2))
     {
-      videoram[0 + i] = 'A';
-      i++;
-    }
-    
+      //vid_buffer = c;
+      videoram[i] = c;
+      videoram[i + 1] = 0x07;
+      i += 2;
+    }    
 }
 
-void printk(char * my_string)
+void printk(char * my_string, unsigned short int start_i)
 {
   unsigned short int i, str_count_i = 0;
-  unsigned char *videoram = (unsigned char *) 0xb8000;
-  while(my_string[str_count_i] != '\0')
+  unsigned char *videoram = (unsigned char *) 0xb8000 + start_i;
+  while((my_string[str_count_i] != '\0') && (str_count_i < 80))
     {
-      //videoram[i] = my_string[str_count_i];
       videoram[i] = my_string[str_count_i];
-      videoram[i + 1] = 0x07;
-      
+      videoram[i + 1] = 0x07;      
+    
       i += 2;
       str_count_i++;
     }
