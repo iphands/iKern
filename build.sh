3f0e1249f4098c7123c9cf98fd2c0e301ebc2200
Ian Page Hands <iphands@core2.lan>
2009-12-08 19:50:23 -0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -n "Building loader: "
+nasm -f elf -o loader.o loader.s
+echo -e "done\n\n"
+
+echo -n "Building kernel: "
+gcc -o kernel.o -c kernel.c -Wall -Wextra -nostdlib -nostartfiles -nodefaultlibs
+echo -e "done\n\n"
+
+echo -n "Building linker: "
+ld -T linker.ld -o kernel.bin loader.o kernel.o
+echo -e "done\n\n"
+
+echo -n "Building floppy image: "
+dd if=/dev/zero of=./pad bs=1 count=750
+cat  grub-stage/stage1 grub-stage/stage2 pad kernel.bin > floppy.img
+#cat  grub-stage/stage1 grub-stage/stage2 pad > floppy.img
+size=`ls -l floppy.img | awk '{print $5}'`
+
+kern_size=`ls -l kernel.bin | awk '{print $5}'`
+echo "size: $kern_size"
+echo "offset: `expr $kern_size / 512`"
+
+new_size=`expr 1440000 - $size`
+mv floppy.img floppy.img.tmp
+dd if=/dev/zero of=./pad2 bs=1 count=$new_size
+cat floppy.img.tmp pad2 > floppy.img
+echo -e "done\n\n"
+
+qemu-kvm -fda ./floppy.img
---
+++ kernel.c
@@ -0,0 +1,38 @@
+void kmain( void* mbd, unsigned int magic )
+{
+  if ( magic != 0x2BADB002 )
+    {
+      /* Something went not according to specs. Print an error */
+      /* message and halt, but do *not* rely on the multiboot */
+      /* data structure. */      
+      unsigned char *videoram = (unsigned char *) 0xb8000;
+      videoram[0] = 66; /* character 'B' */
+      videoram[1] = 0x07; /* forground, background color. */
+    }
+ 
+  /* You could either use multiboot.h */
+  /* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
+  /* or do your offsets yourself. The following is merely an example. */ 
+  char * boot_loader_name =(char*) ((long*)mbd)[16];
+ 
+  /* Print a letter to screen to see everything is working: */
+  unsigned char *videoram = (unsigned char *) 0xb8000;
+
+  //videoram[0] = 65; /* character 'A' */
+  printk("Hello World");
+  videoram[1] = 0x07; /* forground, background color. */
+ 
+  /* Write your kernel here. */
+}
+
+void printk(char * my_string)
+{
+  unsigned short int i = 0;
+  unsigned char *videoram = (unsigned char *) 0xb8000;
+  while(my_string[i] != '\0')
+    {
+      videoram[0] = my_string[i];
+      i++;
+    }
+  return;
+}
