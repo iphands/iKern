ad783613b7bf2db8764ed1f56076f441b1dcf016
Ian Page Hands <iphands@gmail.com>
2009-12-10 22:13:38 -0500

    kernel.c: just testing

--- kernel.c
+++ kernel.c
@@ -25,39 +25,41 @@ void kmain( void* mbd, unsigned int magic )
   /* Write your kernel here. */
   flush_video(' ');
   
-  
+  push_line(boot_loader_name);
   push_line("Hello World");
   push_line("This is a test");
   push_line("This is a test");
   push_line("Hello World");
 
   printk("iKern> ", (80 * 24) * 2);
+  printk("iKern> ", (80 * 24) * 2);
+
 
   /*
-  while (1)
+    while (1)
     {
-      change_term_color(0x07);
-      2 * 4 *2 +4 +2;
-      change_term_color(0x1f);
-      2 * 4 *2 +4 +2;
-      change_term_color(0x2a);
-      2 * 4 *2 +4 +2;
+    change_term_color(0x07);
+    2 * 4 *2 +4 +2;
+    change_term_color(0x1f);
+    2 * 4 *2 +4 +2;
+    change_term_color(0x2a);
+    2 * 4 *2 +4 +2;
+    }
+
+    while (1)
+    {
+    int j = 0;
+    unsigned char * videoram = (unsigned char *) 0xb8000;
+    unsigned char * ram = (unsigned char *) 0x00000;
+    for (int i = 0; i < (1920 * 2); i += 2)
+    {
+    videoram[i] = ram[j];
+    videoram[i + 1] = 0x07;
+
+    j++;
+    }
     }
   */
-
-  while (1)
-    {
-      int j = 0;
-      unsigned char * videoram = (unsigned char *) 0xb8000;
-      unsigned char * ram = (unsigned char *) 0x00000 + 1920 + 1920;
-      for (int i = 0; i < (1920 * 2); i += 2)
-	{
-	  videoram[i] = ram[j];
-	  videoram[i + 1] = 0x07;
-
-	  j++;
-	}
-    }
 
   //unsigned char *videoram = (unsigned char *) 0xb8000;
   //videoram[1920 * 2] = 'A';
